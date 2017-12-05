0f5192677aff09152b3e52b9a5087fe351103263
Tuhin Bepari <digitaldreams40@gmail.com>
2017-12-05 18:10:51 +0600

    Multiple Update by ModelAdmin actions field

--- blog/admin.py
+++ blog/admin.py
@@ -21,6 +21,19 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ['title', 'body']
     list_display = ['title', 'status', 'published_at']
     list_filter = ['status', 'published_at']
+    date_hierarchy = 'published_at'
+    actions = ['make_published']
+
+    def make_published(self, request, queryset):
+        rows_updated =queryset.update(status='published')
+        if rows_updated == 1:
+            message_bit = "1 post was"
+        else:
+            message_bit = "%s posts were" % rows_updated
+        self.message_user(request, "%s successfully marked as published." % message_bit)
+
+    make_published.short_description = 'Mark selected post as Published'
+
 
 
 admin.site.register(Category, CategoryAdmin)
