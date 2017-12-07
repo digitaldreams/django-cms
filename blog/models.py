17c7ff3cb5f898b2a80d02797bd3a5daa8a395d1
Tuhin Bepari <digitaldreams40@gmail.com>
2017-12-08 02:16:05 +0600

    Adding Meta Class on Post Model

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,12 @@ class Tag(models.Model):
 
 
 class Post(models.Model):
+    # Meta is a special Class which used to decorate model.
+    #  Model specific features e.g. ordering, db_table, indexes,permissions, get_latest_by, abstract are defined here as
+    # Attribute. We can not define this as Model attribute.
+    class Meta:
+        ordering=['-published_at']
+
     statuses = (
         ('published', 'Published'),
         ('pending', 'Pending'),
@@ -43,6 +49,9 @@ class Comment(models.Model):
     user = models.IntegerField(default=None)
     # here we assign related_name=comments so that we can able to access this relation
     # from Post class as Post.comments not Post.comment_set
+    # limit_choices_to keyword are also used here. Which is a filter for a relationship
+    # to_field keyword are used to specify foreign_key name. By default Django will use PK
+
     post = models.ForeignKey(Post, related_name='comments')
     body = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
